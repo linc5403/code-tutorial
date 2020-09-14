ae88fc37f222847ca3314c04d69d418b0a0c953e
lin chuan <linch1982@gmail.com>
2020-09-14 23:53:14 +0800

    update

--- code/js/hello.js
+++ code/js/hello.js
@@ -1,18 +1,32 @@
-var a = "zhangsan";
-console.log(a);
-var b = new Object();
-b.name = a;
-b.age = 12;
-
-b.score = [];
-b.score[0] = 100;
-b.score[1] = 200;
-b.out = function(aaa) {
-  console.log("name: " + b.name);
-  console.log("age: " + b.age);
-  b.score.forEach(element => {
-    console.log(element);
-  });
-  console.log(aaa);
+var a = [];
+for (let i = 0; i < 10; i++) {
+  a.push(Math.floor(Math.random() * 10));
+  // a[i] = Math.floor(Math.random() * 10);
 }
-b.out(a);
+console.log("a :>> ", a);
+
+a.forEach(element => {
+  console.log('element :>> ', element);
+});
+
+// selectSort = function (a) {
+function sort(a) {
+  for (let i = 0; i < a.length-1; i++) {
+    let min = i;
+    for (let j = i + 1; j < a.length; j++) {
+      if (a[j] < a[min]) {
+        min = j;
+      }
+    }
+    if (min != i) {
+      let t = a[i];
+      a[i] = a[min];
+      a[min] = t;
+    }
+  }
+};
+
+// selectSort(a);
+sort(a);
+
+console.log("a :>> ", a);
