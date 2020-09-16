89cdd9695e43ee99c2d06cbcd9562966a0ab1da5
lin chuan <linch1982@gmail.com>
2020-09-16 23:17:35 +0800

    更新计算器简易版

---
+++ code/js/calc.js
@@ -0,0 +1,9 @@
+function run() {
+  var num1 = Number(document.getElementById("num1").value);
+  var num2 = Number(document.getElementById("num2").value);
+  var op = document.getElementById("op").value;
+  var rlt;
+  // your code ...
+
+  document.getElementById("result").value = rlt;
+}
