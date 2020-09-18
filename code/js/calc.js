7076828e0c501f6e5b5bde14b48c4e548c45cfec
linchuan <linch1982@gmail.com>
2020-09-18 08:20:14 +0800

    update

--- code/js/calc.js
+++ code/js/calc.js
@@ -1,9 +1,13 @@
+let runButton = document.getElementById("run");
+
 function run() {
-  var num1 = Number(document.getElementById("num1").value);
-  var num2 = Number(document.getElementById("num2").value);
-  var op = document.getElementById("op").value;
-  var rlt;
+  let num1 = Number(document.getElementById("num1").value);
+  let num2 = Number(document.getElementById("num2").value);
+  let op = document.getElementById("op").value;
+  let rlt;
   // your code ...
 
   document.getElementById("result").value = rlt;
 }
+
+runButton.addEventListener("click", run);
