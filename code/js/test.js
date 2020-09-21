1f2ec20888a2e1b869263ff9f6b30c89f825f54b
lin chuan <linch1982@gmail.com>
2020-09-21 23:48:19 +0800

    update

--- code/js/test.js
+++ code/js/test.js
@@ -11,3 +11,24 @@ class Student {
 
 let s = new Student("wangwu");
 s.intro();
+
+const out = document.getElementById("out");
+
+function print(n) {
+  let str = "";
+  for(let i = 1; i <= n; i++) {
+    // 打印空格
+    // 空格的数目是5-i
+    for (let j = 0; j < n - i; j++) {
+      str += "&nbsp";
+    }
+    // 打印**
+    for (let j = 0; j < 2*i-1; j++) {
+      str += "*";
+    }
+    str += "<br>";
+  }
+  out.innerHTML = str;
+}
+
+print(6);
