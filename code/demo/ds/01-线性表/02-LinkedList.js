ec3d735a76912d01ff835d97a993c63a72b29b96
linchuan <linch1982@gmail.com>
2020-09-23 14:33:20 +0800

    增加链表

---
+++ code/demo/ds/01-线性表/02-LinkedList.js
@@ -0,0 +1,80 @@
+// 单链表
+
+// 定义节点的类(class)
+class Node {
+  constructor(item, next = null) {
+    this.item = item;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+  // 查找指定位置的节点
+  // 注意：findPos函数并没有判断pos是否合法
+  findPos(pos) {
+    let r = this.head;
+    for (let i = 0; i < pos; i++) {
+      r = r.next;
+    }
+    return r;
+  }
+
+  // 在指定位置插入一个节点
+  insert(item, pos) {
+    // 判断pos的有效范围
+    if (pos < 0 || pos > this.length) {
+      return false;
+    }
+    // 初始化Node的操作
+    let newNode = new Node(item);
+    // 如果插入0号位置，表示变更了链表的头结点
+    if (pos == 0) {
+      // 新head插入的节点应该是新的头结点
+      newNode.next = this.head; // 新节点指向现在的头结点
+      this.head = newNode; // 变更链表的头结点为新创建的节点
+      this.length++; // 长度+1 不要忘了
+      return true;
+    }
+    // 找到pos-1编号的节点
+    let pre = this.findPos(pos - 1);
+    // newNode指向现在的pos节点
+    newNode.next = pre.next;
+    // pre指向newNode
+    pre.next = newNode;
+    // 别忘了要改变长度
+    this.length++;
+    return true;
+  }
+
+  // 删除指定位置的节点，返回null或者对应节点里面的元素（item）
+  delete(pos) {
+    // 判断pos的合法性
+    if (pos < 0 || pos >= this.length) {
+      return null;
+    }
+    // 删除头结点
+    if (pos == 0) {
+      let r = this.head; // 保存返回值
+      this.head = r.next; // 头节点变更为之前的1#节点
+      this.length--; // 别忘了长度
+      return r.item;
+    }
+    // 删除其它节点
+    let pre = this.findPos(pos - 1);
+    let r = pre.next;
+    pre.next = pre.next.next;
+    this.length--;
+    return r.item;
+  }
+}
+
+let l = new LinkedList();
+l.insert(10, 0);
+l.insert(20, 0);
+l.insert(30, 1);
+console.log(l);
+console.log(l.insert(50, 10));
