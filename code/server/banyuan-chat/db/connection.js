72e2682b876e34d7c6a2c2aad5ac4998afcd48c7
lin chuan <linch1982@gmail.com>
2020-10-02 11:39:24 +0800

    update

--- code/server/banyuan-chat/db/connection.js
+++ code/server/banyuan-chat/db/connection.js
@@ -3,7 +3,7 @@ var mysql      = require('mysql2');
 
 let connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'root',
+  user     : 'banyuan',
   password : 'asdf1234',
   database : 'chat'
 });
